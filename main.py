f9cacd53f894b56bb8a2bb557629c502edd4e284
JDgomez2002 <gom21429@uvg.edu.gt>
2023-09-19 21:35:39 -0600

    minimizacion

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ with open("expresiones.txt", 'r') as archivo:
 
   for expresion in archivo:
     print("############################")
+
     # infix a postfix
     expresion = expresion.strip() # remover espacios en blanco " "
     expresion_postfix = shunting_yard(expresion, num_de_expresion) # infix a postfix
@@ -34,21 +35,24 @@ with open("expresiones.txt", 'r') as archivo:
     gafico_arbol = graficar_arbol(arbol) # crear grafico
     gafico_arbol.view(filename=f"Arbol {num_de_expresion}", cleanup=True) # Crear y mostrar archivo pdf
 
+    # crear afn
     afn = arbol_a_afn(arbol) # arbol a afn
     afn.estados = EstadoAFN.estados # tomar estados
     afn_graph = graficar_afn(afn.inicio) # graficar AFN
     afn_graph.view(filename=f"AFN {num_de_expresion}", cleanup=True) # crear y mostrar archivo pdf
 
-    alfabeto = obtener_alfabeto(expresion_postfix) # obtiene el alfabeto del afd
+    # crear afd
+    alfabeto = obtener_alfabeto(expresion_postfix) # obtiene el alfabeto de la expresion
     afd = afn_a_afd(alfabeto, afn) # crear afd con su alfabeto
     afd.estados = EstadoAFD.estados # obtiene los estados del afd
     afd_graph = graficar_afd(afd.inicio) # grafica el afd
     afd_graph.view(filename=f"AFD {num_de_expresion}", cleanup=True) # crear y mostrar archivo pdf
 
-    # afd_min = afd_minimizado(alfabeto, afd) # minimizar afd
-    # print(afd_min.estados_aceptacion) # imprimir estados de minimizacion
-    # afdmin_graph = graficar_afd(afd_min.estado_inicial) # graficar afd minimizado
-    # afdmin_graph.view(filename=f"AFD min{num_de_expresion}", cleanup=True) # crear y mostrar archivo pdf
+  # minimizar afd
+    afd_min = afd_minimizado(alfabeto, afd) # minimizar afd
+    print(afd_min.aceptacion) # imprimir estados de minimizacion
+    afdmin_graph = graficar_afd(afd_min.inicio) # graficar afd minimizado
+    afdmin_graph.view(filename=f"AFD min{num_de_expresion}", cleanup=True) # crear y mostrar archivo pdf
 
     print("############################")
 
--- minimizacion.py
+++ minimizacion.py
@@ -1,162 +1,61 @@
-# class EstadoAFD:
-#     contador_estados = 'A'
-#     estados = set()
+from afd import *
 
-#     def __init__(self, subconjunto=set()):
-#         self.nombre = str(EstadoAFD.contador_estados)
-#         EstadoAFD.contador_estados = chr(ord(EstadoAFD.contador_estados) + 1)
-#         EstadoAFD.estados.add(self)
-#         self.subconjunto = subconjunto
+def afd_minimizado(alfabeto, afd):
+  P = [set(afd.aceptacion), afd.estados - set(afd.aceptacion)]
+  
+  W = [set(afd.aceptacion), afd.estados - set(afd.aceptacion)]
 
-#         self.transiciones = {}
-#         self.es_aceptacion = False
+  while W:
+    A = W.pop(0)
+    for char in alfabeto:
+      estado_afectados = set()
+      for estado in afd.estados:
+        if char in estado.transiciones and estado.transiciones[char][0] in A:
+          estado_afectados.add(estado)
 
-# class AFD:
-#     def __init__(self):
-#         self.estado_inicial = None
-#         self.estados_aceptacion = set()
-#         self.estados = set()
+      for Y in P:
+        interseccion = Y.intersection(estado_afectados)
+        diferencia = Y.difference(estado_afectados)
 
-# def cerradura_epsilon_estado(estados, visitado=None):
-#     if visitado is None:
-#         visitado = set()
+        if interseccion and diferencia:
+          P.remove(Y)
+          P.append(interseccion)
+          P.append(diferencia)
 
-#     if estados in visitado:
-#         return
-        
-#     visitado.add(estados)
+          if Y in W:
+            W.remove(Y)
+            W.append(interseccion)
+            W.append(diferencia)
 
-#     for symbol, next_states in estados.transiciones.items():
-#         for next_state in next_states:
-#             if symbol == 'ε':
-#                 cerradura_epsilon_estado(next_state,visitado)
+          else:
+            W.append(interseccion)
+            W.append(diferencia)
 
-# def cerradura_epsilon(S):
-#     res = set()
-#     for state in S:
-#         res = res.union(cerradura_epsilon_estado(state))
+  afd_minimizado = AFD()
+  
+  estado_mapeado = {}
+  for nuevo_grupo in P:
+    nuevo_estado = EstadoAFD()
+    for estado_anterior in nuevo_grupo:
+      estado_mapeado[estado_anterior] = nuevo_estado
+      if estado_anterior.es_aceptacion:
+        nuevo_estado.is_accept = True
+    afd_minimizado.estados.add(nuevo_estado)
+  
+  afd_minimizado.inicio = estado_mapeado[afd.inicio]
+  
+  for estado_anterior in afd.estados:
+    nuevo_estado = estado_mapeado.get(estado_anterior)
+    if nuevo_estado:
+      for simbolo, transicion_anterior in estado_anterior.transiciones.items():
+        estado_objetivo_anterior = transicion_anterior[0]
+        estado_objetivo_actual = estado_mapeado.get(estado_objetivo_anterior)
 
-# def alfabeto_regex(postfijo):
-#     alfabeto = set()
-#     reservadas = ['|', '*', '.', 'ε']
-
-#     for caracter in postfijo:
-#         if caracter not in reservadas:
-#             alfabeto.add(caracter)
-
-#     return alfabeto
-
-# def mover(S,c):
-#     res = set()
-    
-#     for state in S:
-#         for symbol,next_states in state.transiciones.items():
-#             for next_state in next_states:
-#                 if symbol == c:
-#                     res.add(next_state)
-
-# def afn_a_afd(alfabeto, afn):
-#     F = set()
-#     F.add(afn.aceptacion)
-#     estados = []
-
-#     So = set()
-#     So.add(afn.inicio)
-
-#     afd = AFD()
-
-#     estados.append(EstadoAFD(subconjunto=cerradura_epsilon(So)))
-#     afd.estado_inicial = estados[0]
-
-#     if afn.aceptacion in estados[0].subconjunto:
-#         afd.estados_aceptacion.add(estados[0])
-#         estados[0].es_aceptacion = True
-
-#     contador = 0
-#     nuevos_estados = 0
-
-#     while contador != nuevos_estados or (contador == 0 and nuevos_estados == 0):
-#         if nuevos_estados != 0:
-#             contador += 1
-
-#         for simbolo in alfabeto:
-#             cambio = False
-#             subconjunto = cerradura_epsilon(mover(estados[contador].subconjunto, simbolo))
-
-#             for estado in estados:
-#                 if estado.subconjunto == subconjunto:
-#                     estados[contador].transiciones[simbolo] = [estado]
-#                     cambio = True
-#                     break
-
-#             if not cambio:
-#                 nuevo_estado = EstadoAFD(subconjunto)
-
-#                 if afn.aceptacion in subconjunto:
-#                     afd.estados_aceptacion.add(nuevo_estado)
-#                     nuevo_estado.es_aceptacion = True
-
-#                 estados[contador].transiciones[simbolo] = [nuevo_estado]
-#                 estados.append(nuevo_estado)
-#                 nuevos_estados += 1
-
-#     return afd
-
-# def afd_minimizado(alfabeto, afd):
-#     P = [set(afd.estados_aceptacion), afd.estados - set(afd.estados_aceptacion)]
-
-#     W = [set(afd.estados_aceptacion), afd.estados - set(afd.estados_aceptacion)]
-
-#     while W:
-#         A = W.pop(0)
-#         for c in alfabeto:
-#             estados_afectados = set()
-#             for estado in afd.estados:
-#                 if c in estado.transiciones and estado.transiciones[c][0] in A:
-#                     estados_afectados.add(estado)
-
-#             for Y in P:
-#                 interseccion = Y.intersection(estados_afectados)
-#                 diferencia = Y.difference(estados_afectados)
-
-#                 if interseccion and diferencia:
-#                     P.remove(Y)
-#                     P.append(interseccion)
-#                     P.append(diferencia)
-
-#                     if Y in W:
-#                         W.remove(Y)
-#                         W.append(interseccion)
-#                         W.append(diferencia)
-#                     else:
-#                         W.append(interseccion)
-#                         W.append(diferencia)
-
-#     afd_min = AFD()
-
-#     mapeo_estados = {}
-#     for grupo_nuevo_estado in P:
-#         nuevo_estado = EstadoAFD()
-#         for estado_antiguo in grupo_nuevo_estado:
-#             mapeo_estados[estado_antiguo] = nuevo_estado
-#             if estado_antiguo.es_aceptacion:
-#                 nuevo_estado.es_aceptacion = True
-#         afd_min.estados.add(nuevo_estado)
-
-#     afd_min.estado_inicial = mapeo_estados[afd.estado_inicial]
-
-#     for estado_antiguo in afd.estados:
-#         nuevo_estado = mapeo_estados.get(estado_antiguo)
-#         if nuevo_estado:
-#             for simbolo, transiciones_antiguas in estado_antiguo.transiciones.items():
-#                 estado_destino_antiguo = transiciones_antiguas[0]
-#                 estado_destino_nuevo = mapeo_estados.get(estado_destino_antiguo)
-
-#                 if estado_destino_nuevo:
-#                     nuevo_estado.transiciones[simbolo] = [estado_destino_nuevo]
-#                 else:
-#                     print(f'{simbolo}')
-#         else:
-#             print(f'{estado_antiguo}')
-
-#     return afd_min
+        if estado_objetivo_actual:
+          nuevo_estado.transiciones[simbolo] = [estado_objetivo_actual]
+        else:
+          print('{simbolo}')
+    else:
+      print('{estado_anterior}')
+  
+  return afd_minimizado
